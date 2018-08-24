745d29224a525ebee3c668b1145fe7978136cc05
Maor Oz <maoroz92@gmail.com>
2018-08-24 19:05:10 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Router, Link } from 'react-static'
+import styled, { injectGlobal } from 'styled-components'
+import { hot } from 'react-hot-loader'
+import WebFont from 'webfontloader'
+import resetCss from 'styled-reset'
+//
+import Routes from 'react-static-routes'
+
+WebFont.load({
+  google: {
+    families: ['M PLUS Rounded 1c:400,700&text=אבגהוזחטיכלמנסעפצקרשת']
+  }
+})
+injectGlobal`
+ ${resetCss}
+  body {
+    font-family: 'M PLUS Rounded 1c';
+    margin: 0;
+    padding: 0;
+  }
+`
+
+const AppStyles = styled.div`
+  a {
+    text-decoration: none;
+    color: #108db8;
+    font-weight: bold;
+  }
+
+  nav {
+    width: 100%;
+    background: #108db8;
+
+    a {
+      color: white;
+      padding: 1rem;
+      display: inline-block;
+    }
+  }
+
+  .content {
+    padding: 1rem;
+  }
+
+  img {
+    max-width: 100%;
+  }
+`
+
+const App = () => (
+  <Router>
+    <AppStyles>
+      <div className="content">
+        <Routes />
+      </div>
+    </AppStyles>
+  </Router>
+)
+
+export default hot(module)(App)
